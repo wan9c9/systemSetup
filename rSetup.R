701786f3b32fa5ecc81561d6b040596addd89d45
Chao Wang <wan9c9@users.noreply.github.com>
2015-09-22 16:48:28 -0500

    Update rSetup.R

--- rSetup.R
+++ rSetup.R
@@ -1,8 +1,5 @@
 download.file("http://www.lepem.ufc.br/jaa/vimcom.plus_0.9-93.tar.gz", destfile = "vimcom.plus_0.9-93.tar.gz")
 install.packages("vimcom.plus_0.9-93.tar.gz", type = "source", repos = NULL)
-
-
-
 download.file("http://www.lepem.ufc.br/jaa/colorout_1.0-2.tar.gz", destfile = "colorout_1.0-2.tar.gz")
 install.packages("colorout_1.0-2.tar.gz", type = "source", repos = NULL)
 
@@ -12,7 +9,17 @@ install.packges("devtools",dependencies=TRUE)
 library("devtools")
 devtools::install <- github("klutometis/roxygen")
 library(roxygen2)
+
+#for matrix computation
 install.packges("matrixStats",dependencies=TRUE)
 install.packges("matrixcalc",dependencies=TRUE)
+
+
 install.packges("TSA",dependencies=TRUE)
+
+install.packges("tmvtnorm",dependencies=TRUE)
+install.packges("xts",dependencies=TRUE)
+install.packges("zoo",dependencies=TRUE)
+
+
 
