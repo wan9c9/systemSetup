5223277b3970820f2772faa694ce0d028e9efea2
Chao Wang <wan9c9@users.noreply.github.com>
2015-09-22 10:45:19 -0500

    Create rSetup.R
    This is the file used to set up my R environment.

---
+++ rSetup.R
@@ -0,0 +1,18 @@
+download.file("http://www.lepem.ufc.br/jaa/vimcom.plus_0.9-93.tar.gz", destfile = "vimcom.plus_0.9-93.tar.gz")
+install.packages("vimcom.plus_0.9-93.tar.gz", type = "source", repos = NULL)
+
+
+
+download.file("http://www.lepem.ufc.br/jaa/colorout_1.0-2.tar.gz", destfile = "colorout_1.0-2.tar.gz")
+install.packages("colorout_1.0-2.tar.gz", type = "source", repos = NULL)
+
+install.packages("setwidth",dependencies=TRUE)
+install.packges("rugarch",dependencies=TRUE)
+install.packges("devtools",dependencies=TRUE)
+library("devtools")
+devtools::install <- github("klutometis/roxygen")
+library(roxygen2)
+install.packges("matrixStats",dependencies=TRUE)
+install.packges("matrixcalc",dependencies=TRUE)
+install.packges("TSA",dependencies=TRUE)
+
