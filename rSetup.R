80c07da5996bf697a5972b1b238493afb04edcde
Chao Wang <wan9c9@users.noreply.github.com>
2015-09-22 16:50:46 -0500

    Update rSetup.R

--- rSetup.R
+++ rSetup.R
@@ -4,22 +4,22 @@ download.file("http://www.lepem.ufc.br/jaa/colorout_1.0-2.tar.gz", destfile = "c
 install.packages("colorout_1.0-2.tar.gz", type = "source", repos = NULL)
 
 install.packages("setwidth",dependencies=TRUE)
-install.packges("rugarch",dependencies=TRUE)
-install.packges("devtools",dependencies=TRUE)
+install.packages("rugarch",dependencies=TRUE)
+install.packages("devtools",dependencies=TRUE)
 library("devtools")
 devtools::install <- github("klutometis/roxygen")
 library(roxygen2)
 
 #for matrix computation
-install.packges("matrixStats",dependencies=TRUE)
-install.packges("matrixcalc",dependencies=TRUE)
+install.packages("matrixStats",dependencies=TRUE)
+install.packages("matrixcalc",dependencies=TRUE)
 
 
-install.packges("TSA",dependencies=TRUE)
+install.packages("TSA",dependencies=TRUE)
 
-install.packges("tmvtnorm",dependencies=TRUE)
-install.packges("xts",dependencies=TRUE)
-install.packges("zoo",dependencies=TRUE)
+install.packages("tmvtnorm",dependencies=TRUE)
+install.packages("xts",dependencies=TRUE)
+install.packages("zoo",dependencies=TRUE)
 
 
 
