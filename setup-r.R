4ec80a96215e6b718e974c5d1214b0110a33aaa2
Chao Wang <wan9c9@users.noreply.github.com>
2016-03-08 16:42:18 -0600

    Update setup-r.R

--- setup-r.R
+++ setup-r.R
@@ -1,26 +1,28 @@
-download.file("http://www.lepem.ufc.br/jaa/vimcom.plus_0.9-93.tar.gz", destfile = "vimcom.plus_0.9-93.tar.gz")
-install.packages("vimcom.plus_0.9-93.tar.gz", type = "source", repos = NULL)
-download.file("http://www.lepem.ufc.br/jaa/colorout_1.0-2.tar.gz", destfile = "colorout_1.0-2.tar.gz")
-install.packages("colorout_1.0-2.tar.gz", type = "source", repos = NULL)
+install.packages("devtools",dependencies=TRUE)
+
+
+library("devtools")
+devtools::install_github("jalvesaq/VimCom")
+devtools::install_github("klutometis/roxygen")
+devtools::install_github("jalvesaq/colorout")
+
+
 
 install.packages("setwidth",dependencies=TRUE)
-install.packages("rugarch",dependencies=TRUE)
-install.packages("devtools",dependencies=TRUE)
-library("devtools")
-devtools::install <- github("klutometis/roxygen")
-library(roxygen2)
 
+install.packages("emulator",dependencies=TRUE)
+
+
+
+
+#library(roxygen2)
 #for matrix computation
-install.packages("matrixStats",dependencies=TRUE)
-install.packages("matrixcalc",dependencies=TRUE)
+install.packages(c("matrixStats","matrixcalc"),dependencies=TRUE)
+
+install.packages(c('tmvtnorm','mvtnorm','xts','zoo','nlme'),dependencies=TRUE)
 
 
 install.packages("TSA",dependencies=TRUE)
-
-install.packages("tmvtnorm",dependencies=TRUE)
-install.packages("xts",dependencies=TRUE)
-install.packages("zoo",dependencies=TRUE)
-install.packages("emulator",dependencies=TRUE)
-
+install.packages("rugarch",dependencies=TRUE)
 
 
